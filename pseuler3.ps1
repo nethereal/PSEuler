cbcc4a18c954eca81e5613dd1c2829be4c0d8e5d
nethereal <jcourtes@gmail.com>
2017-12-29 18:22:18 -0500

    added initial files

---
+++ pseuler3.ps1
@@ -0,0 +1,38 @@
+﻿# Project Euler - 3
+#
+# The prime factors of 13195 are 5, 7, 13 and 29.
+# 
+# What is the largest prime factor of the number 600851475143 ?
+
+$upperlimit = [uint64]600851475143
+$result = $null
+
+function Test-Prime {
+    param(
+        [Parameter(ValueFromPipeline=$true)]
+        [uint64]$number
+        )
+    Process {
+        $prime = $true;
+        if ($number -eq 1) { $prime = $false; }
+        if ($number -gt 3) {
+            $sqrt = [math]::Sqrt($number); 
+            for($i = 2; $i -le $sqrt; $i++) {
+                if ($number % $i -eq 0) {
+                    $prime = $false;
+                    break;
+                }
+            }
+        }
+        return $prime;
+    }
+}
+function Get-Factor ($a) {
+    1..[Math]::Sqrt($a) `
+        | Where-Object { $a % $_ -eq 0 } `
+        | ForEach-Object { $_; $a / $_ } `
+        | Sort-Object -Unique
+}
+
+(Get-Factor $upperlimit) | % { if (Test-Prime $_) { $result = $_ } }
+$result
---
+++ pseuler4.ps1
@@ -0,0 +1,20 @@
+﻿# Project Euler - 3
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+# 
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+$numberA = 999
+$numberB = 999
+
+function isPalindrome($number) {
+    $str1 = [string]$number
+    $str2 = [string]$number
+    $str2 = $str2.ToCharArray()
+    $str2 = $str2 -join ([array]::Reverse($str2))
+    if ("$str1" -eq "$str2") { $true } 
+    else { $false }
+}
+
+for ($a = $numberA; $a -gt 0; $a = $a - 1) {
+
+}
